7f04fa15a87c94ec2dcbf1d9fd5d9558eececa79
EduardoPH <dudu19424irma@gmail.com>
2021-09-23 10:30:55 -0300

    denuncia-detalhada 23/09

---
+++ site/src/components/details-denuncia/index.js
@@ -0,0 +1,11 @@
+import { Container } from "./styled";
+
+export default function Pag() {
+    return(
+        <Container>
+            <div className="box">
+                <div className="mensagem"> <img src="/assets/images/denuncia-details/setinha/svg"alt=""/> </div>
+            </div>
+        </Container>
+    )
+}
--- site/src/routes.js
+++ site/src/routes.js
@@ -4,6 +4,7 @@ import Home from "./components/menu/index";
 import Estatisticas from './pages/estatisticas';
 import Administrador from './pages/Administrador';
 import DenunciasRecents from './pages/Denuncias-recentes'
+import DenunciaDetalhada from './pages/denuncia-detalhe'
 
 
 export default function Routes(){
@@ -14,6 +15,7 @@ export default function Routes(){
                 <Route path="/estatisticas"  component={Estatisticas} />
                 <Route path="/administrador"  component={Administrador} />
                 <Route path="/denuncia-recents"  component={DenunciasRecents} />
+                <Route path="/denuncia-details" component={DenunciaDetalhada}/>
             </Switch>
         </BrowserRouter>
 
