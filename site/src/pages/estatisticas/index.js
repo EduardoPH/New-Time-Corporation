e13f3cfe0ade506ab8cd8d432cc956094e2aa1c8
heloysa <santos.heloysaarruda@gmail.com>
2021-09-23 21:58:31 -0300

    carrossel estatisticas

--- site/src/pages/estatisticas/index.js
+++ site/src/pages/estatisticas/index.js
@@ -1,14 +1,50 @@
 import Containers from './styled.js';
 import Menu from '../../components/menu'
 
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
+
+class DemoCarousel extends Component {
+    render() {
+        return (
+            <Carousel>
+                <div>
+                    <img src="assets/1.jpeg" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="assets/2.jpeg" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="assets/3.jpeg" />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
+        );
+    }
+};
+
+ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
+
+
+
+
+
+
+
 export default function Estatisticas (){
     return(
         <Containers>
             <Menu/>
             <div class="dados">
             <div class="titulo"> Estatísticas </div>
-            <div class="caixa">
-                <div class="setEsquerda"><img src="/assets/images/estatisticas/setEsquerda.svg" alt=""/></div>
+            <div class="caix">
+              <div class="setEsquerda"><img src="/assets/images/estatisticas/setEsquerda.svg" alt=""/></div>
+              <div class="caixa">
                 <div class="centro">
                     <div class="subT">Bairros com maior número de denúncias</div>
                     <div class="tabLad">
@@ -16,6 +52,7 @@ export default function Estatisticas (){
                         <div class="dados"></div>
                     </div>
                 </div>
+              </div>
                 <div class="setDireita"><img src="/assets/images/estatisticas/setDireita.svg" alt=""/></div>
             </div>
 
