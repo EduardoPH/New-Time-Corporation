6a71509e4f30cc9da2502d37b764cae35d566584
heloysa <santos.heloysaarruda@gmail.com>
2021-10-01 18:45:42 -0300

    components home

--- site/src/pages/home/index.js
+++ site/src/pages/home/index.js
@@ -22,7 +22,7 @@ export default function Home() {
                 </div>       
            </div>
             
-            <Home/>
+            <HomeC/>
             
 
        {/*   <div className="box2"> 
