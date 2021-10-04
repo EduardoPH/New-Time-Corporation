a575ef7a674eccab10cc6fab0149a6eb5d6ce9c1
EduardoPH <dudu19424irma@gmail.com>
2021-10-04 15:26:00 -0300

    ok frei dudu

--- site/src/pages/Denuncias-recentes/index.js
+++ site/src/pages/Denuncias-recentes/index.js
@@ -1,19 +1,40 @@
 import { Container } from "./styled";
 import Menu from '../../components/menu'
 import Mensagem from '../../components/recents-mensagem'
+import { useState } from "react";
 
 export default function Details() {
+
+    const [denuncia, setDenuncia] = useState([])
+
+    function ApiResponse(){
+        let r = [
+            {
+                name: "Dudu",
+                msg: "opaaa testando"
+            }, 
+            {
+                name: "Erik",
+                msg: "opaaa testando"
+            }, 
+            {
+                name: "Jhow",
+                msg: "opaaa testando"
+            },
+        ]
+
+        setDenuncia(r)
+    }
+
     return(
         <Container>
             <Menu/>
             <div className="rc-titulo"> Denúncias Recentes </div>
             <div className="rc-conteudo">
                 <div className="rc-denuncias"> 
-                    <Mensagem/>
-                    <Mensagem/>
-                    <Mensagem/>
-                    <Mensagem/>
-                    <Mensagem/>
+                    {denuncia.map(item => {
+                        <Mensagem  denuncia={item} />
+                    })}
                 </div>
             </div>
         </Container>
