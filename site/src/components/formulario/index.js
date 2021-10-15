36cb9e38f2762df08b3fa9441727329f59829ba4
EduardoPH <dudu19424irma@gmail.com>
2021-10-14 22:12:08 -0300

    ok

--- site/src/components/formulario/index.js
+++ site/src/components/formulario/index.js
@@ -3,10 +3,7 @@ import { ButtonStyle } from "./styled";
 export default function App(props){
     return(
         <ButtonStyle> {props.valor} </ButtonStyle>
-<<<<<<< HEAD
+
     );
-} 
-=======
-    )
 }
->>>>>>> parent of 84357a3... ok
+
