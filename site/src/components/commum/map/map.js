2939bce08e39774c89c130a46e06332fd064936a
Erik Benito <ErikBenito_2017@outlook.com>
2021-11-02 16:48:13 -0300

    config map

--- site/src/components/commum/map/map.js
+++ site/src/components/commum/map/map.js
@@ -15,15 +15,14 @@ export class  Mapa extends Component{
 
   constructor(){
     super();
-    this.state={lat: -21.5613462, lng: -42.6564872, end: ""}
+    this.state={lat: -21.5613462, lng: -42.6564872, end: "", late: -0, lngo: -0}
   }
 
    loc(end) {
     Geocode.fromAddress(end).then(
       (response) => {
         const { lat, lng } = response.results[0].geometry.location;
-        this.setState({...this.state, lat: lat, lng: lng})
-        console.log(this.state.lat, this.state.lng)
+        this.setState({...this.state, late: lat, lngo: lng})
       },
       (error) => {
         console.error(error);
@@ -34,6 +33,12 @@ export class  Mapa extends Component{
 
   render(){
     let r = this.props.info === undefined ? "" : this.props.info
+    let p = this.state.late
+
+    if(p !== -0){
+      this.props.loca({lat: this.state.late, lng: this.state.lngo})
+    }
+
     return(
       <div >
         <div style={{display: 'flex', flexDirection: 'row'}}>
@@ -44,7 +49,7 @@ export class  Mapa extends Component{
         <Map 
           google={this.props.google}
           zoom={16}
-          center={{lat: r.lat, lng: r.lgn}}
+          center={p === -0 ? {lat: r.lat, lng: r.lgn} : {lat: this.state.late, lng: this.state.lngo}}
           styles={MapStyle}
           disableDefaultUI={true}
           containerStyle = {containerStyle}
