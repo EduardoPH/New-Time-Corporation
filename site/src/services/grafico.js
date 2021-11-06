d832ea5e8e3d53be39c5b2ee96d10e8e96aecef1
Erik Benito <ErikBenito_2017@outlook.com>
2021-11-06 12:54:49 -0300

    ok api

--- site/src/services/grafico.js
+++ site/src/services/grafico.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = new axios.create({
-    baseURL: 'http://localhost:3030/estatisticas'
+    baseURL: 'https://api-ntc.herokuapp.com/estatisticas'
 })
 
 export default class Api{
